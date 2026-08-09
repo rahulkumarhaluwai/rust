6ab0c796d45c7e5adc3a161fffd8f1e11483fa31
rahulkumarhaluwai <rahulkumarhaluwai@gmail.com>
2026-08-09 10:03:56 +0000

    references & borrowing

---
+++ ownership101/target/debug/deps/ownership101.d
@@ -0,0 +1,5 @@
+D:\Projects\rust\ownership101\target\debug\deps\ownership101.d: src\main.rs
+
+D:\Projects\rust\ownership101\target\debug\deps\ownership101.exe: src\main.rs
+
+src\main.rs:
---
+++ ownership101/target/debug/ownership101.d
@@ -0,0 +1,1 @@
+D:\Projects\rust\ownership101\target\debug\ownership101.exe: D:\Projects\rust\ownership101\src\main.rs
